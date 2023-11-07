f9d45409ef813f2d2df750597892c7cb31e9d29f
Paulo Rosado <paulohenrique110803@gmail.com>
2023-11-07 20:01:20 -0300

    jogoDaVelha

---
+++ JogoDaVelha.py
@@ -0,0 +1,72 @@
+import os
+os.system('cls')
+
+matriz = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]
+
+def printarMatriz(matriz):
+    for i in range(3):
+        for j in range(3):
+            print(f"{matriz[i][j]}", end = " ")
+        print()
+
+def registrarJogada(jogada):
+    for i in range(3):
+        for j in range(3):
+            if jogada == jogadaX:
+                if int(jogadaX[0]) == i+1 and int(jogadaX[1]) == j+1:
+                    matriz[i][j] = 'X'
+            elif jogada == jogadaO:
+                if int(jogadaO[0]) == i+1 and int(jogadaO[1]) == j+1:
+                    matriz[i][j] = 'O'
+
+def analisarJogo(matriz, jogada):
+    for i in range(3):
+        for j in range(3):
+            
+            if jogada == jogadaX: 
+                for z in range(3):
+                    coluna = []
+                    if matriz[i][z] not in coluna: 
+                        coluna.append(matriz[i][z])
+                    if coluna.count('X') == 3:
+                        print()
+                        print(f"X venceu!")
+                        return 'end'
+                if matriz[i].count('X') == 3: #Linha
+                    print()
+                    print(f"X venceu!")
+                    return 'end' 
+
+            
+            elif jogada == jogadaO:
+                if matriz[i].count('O') == 3:
+                    print()
+                    print(f"O venceu!")
+                    return 'end'
+
+rodada = 0
+
+while True:
+    if rodada > 0:
+        print()
+    jogadaX = input("Digite respectivamente a linha (l) e a coluna (c) em que deseja jogar 'X' no formato [lc]: ").upper()
+    print()
+    registrarJogada(jogadaX)
+    printarMatriz(matriz)
+    rodada += 1
+
+    if rodada >= 5:
+        if analisarJogo(matriz, jogadaX) == 'end':
+            break
+
+    
+    print()
+    jogadaO = input("Digite respectivamente a linha (l) e a coluna (c) em que deseja jogar 'O' no formato [lc]: ").upper()
+    print()
+    registrarJogada(jogadaO)
+    printarMatriz(matriz)
+    rodada += 1
+
+    if rodada >= 5:
+        if analisarJogo(matriz, jogadaO) == 'end':
+            break
